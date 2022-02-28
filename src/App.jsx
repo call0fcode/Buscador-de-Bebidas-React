ddcac64bc7bd511c46448293c9ad5164a8a0a9c3
Juan Gómez Carrillo <juan.gomez@interficie.com>
2022-02-28 00:32:22 -0500

    Add some new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
-  return <h1>Bebidas</h1>;
+  return (
+    <>
+      <Header />
+
+      <div className="container mt-5">
+        <div className="row">
+          <Formulario />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
