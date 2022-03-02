9e5842666b602752568164683b8b9ebe17abc992
Juan Gómez Carrillo <juan.gomez@interficie.com>
2022-03-02 14:25:06 -0500

    Add form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Formulario = () => {
-  return <h1>Formulario</h1>;
+  return (
+    <form className="col-12">
+      <fieldset className="text-center mb-4">
+        <legend>Busca bebidas por Categoría o Ingrediente</legend>
+      </fieldset>
+
+      <div className="d-grid gap-2 d-md-flex gap-md-0 row">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Buscar por ingrediente"
+          />
+        </div>
+
+        <div className="col-md-4">
+          <select name="categoria" className="form-control">
+            <option value="">-- Selecciona Categoría --</option>
+          </select>
+        </div>
+
+        <div className="col-md-4 d-grid">
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Formulario;
