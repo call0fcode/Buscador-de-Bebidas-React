e603a73258a0404de8a81bf124e1869d8a99071f
Juan Gómez Carrillo <juan.gomez@interficie.com>
2022-03-02 17:28:09 -0500

    Get recipes from API

--- src/context/RecetasContext.jsx
+++ src/context/RecetasContext.jsx
@@ -1,13 +1,25 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 export const RecetasContext = createContext();
 
 const RecetasProvider = ({ children }) => {
   const [recetas, setRecetas] = useState([]);
-  const [busquedaRecetas, setBusquedaRecetas] = useState({
-    nombre: "",
-    categoria: "",
-  });
+  const [busquedaRecetas, setBusquedaRecetas] = useState({});
+
+  const { nombre, categoria } = busquedaRecetas;
+
+  useEffect(() => {
+    if (Object.keys(busquedaRecetas).length !== 0) {
+      // Consulta la API una vez se reciben los datos desde el form.
+      const obtenerRecetas = async () => {
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&c=${categoria}`;
+        const resultados = await axios.get(url);
+        setRecetas(resultados.data.drinks);
+      };
+      obtenerRecetas();
+    }
+  }, [busquedaRecetas]);
 
   return (
     <RecetasContext.Provider value={{ setBusquedaRecetas }}>
