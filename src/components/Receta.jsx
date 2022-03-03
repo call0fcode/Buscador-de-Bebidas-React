9726f00245b4d9e423f4e1515e16dade75594aa5
Juan Gómez Carrillo <juan.gomez@interficie.com>
2022-03-03 12:33:19 -0500

    Add modal basic structure

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,8 +1,26 @@
 import React, { useContext } from "react";
 import { ModalContext } from "../context/ModalContext";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Receta = ({ receta }) => {
   const { setIdReceta } = useContext(ModalContext);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <div className="col-md-4 mb-3">
@@ -20,8 +38,21 @@ const Receta = ({ receta }) => {
             value="Ver Receta"
             onClick={() => {
               setIdReceta(receta.idDrink);
+              handleOpen();
             }}
           />
+
+          <Modal
+            open={open}
+            onClose={() => {
+              handleClose();
+              setIdReceta(null);
+            }}
+          >
+            <Box sx={style}>
+              <h1>Aquí datos de receta</h1>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
