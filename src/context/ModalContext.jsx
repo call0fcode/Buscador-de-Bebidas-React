c5e9155e15dee2fc6f66aaed244d205464c837f0
Juan Gómez Carrillo <juan.gomez@interficie.com>
2022-03-03 10:33:10 -0500

    Get recipe from API by drink ID

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -5,6 +5,24 @@ export const ModalContext = createContext();
 
 const ModalProvider = ({ children }) => {
   const [idReceta, setIdReceta] = useState(null);
+  const [receta, setReceta] = useState({});
+
+  // Llamar la API una vez que tenemos un ID de receta.
+  useEffect(() => {
+    // Evita ejecución en el primer render ya que no hay receta seleccionada.
+    if (!idReceta) return;
+
+    const obtenerReceta = async () => {
+      try {
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`;
+        const resultado = await axios.get(url);
+        setReceta(resultado.data.drinks[0]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    obtenerReceta();
+  }, [idReceta]);
 
   return (
     <ModalContext.Provider value={{ setIdReceta }}>
